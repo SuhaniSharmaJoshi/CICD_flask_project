01371c98e80c219ea43037164e9d8945dcf984f2
Suhani Joshi <suhanijoshi@suhanis-air.lan>
2026-03-12 12:59:35 -0500

    added nginx

--- app_infra/user_data.sh
+++ app_infra/user_data.sh
@@ -15,10 +15,35 @@ systemctl enable docker
 usermod -aG docker ec2-user
 
 #login to ECR
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 455025093404.dkr.ecr.us-east-1.amazonaws.com
+#aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 455025093404.dkr.ecr.us-east-1.amazonaws.com
 
 #pull docker image
-docker pull 455025093404.dkr.ecr.us-east-1.amazonaws.com/cicd-python-app:latest
+#docker pull 455025093404.dkr.ecr.us-east-1.amazonaws.com/cicd-python-app:latest
 
 #run container
-docker run -d -p 80:5000 455025093404.dkr.ecr.us-east-1.amazonaws.com/cicd-python-app:latest
+#docker run -d -p 80:5000 455025093404.dkr.ecr.us-east-1.amazonaws.com/cicd-python-app:latest
+
+#install Nginx
+sudo amazon-linux-extras install nginx1 -y
+sudo systemctl enable nginx
+sudo systemctl start nginx
+
+#Nginx Reverse Proxy Default COnfig
+sudo bash -c 'cat > /etc/nginx/conf.d/cicd-app.conf <<EON
+server {
+    listen 80;
+    server_name _;
+
+    location / {
+        proxy_pass http://127.0.0.1:5000;  # Docker container port
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        }
+}
+EON'
+
+#Test Nginx config and restart
+
+sudo nginx -t
+sudo systemctl restart nginx
