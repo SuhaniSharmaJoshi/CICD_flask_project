f7542daa253ae95f9429261dca1b02ea2a4b09e3
Suhani Joshi <suhanijoshi@suhanis-air.lan>
2026-03-12 14:10:32 -0500

    updated app.py

--- app/app.py
+++ app/app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CI/CD Pipeline is Working"
+    #get headers sent by nginx
+    client_ip = request.headers.get('X-Real-IP','Not set')
+    forwarded_for = request.headers.get('Host','Not set')
+    host = request.headers.get('Host', 'Not set')
+
+    return f"""
+    <h1>Welcome to CICD App!</h1>
+    <p>This request is being forwarded through <b>Nginx Reverse Proxy</b>.</p>
+    <p><b>Host:</b> {host}</p>
+    <p><b>Client IP:</b> {client_ip}</p>
+    <p><b>X-Forwarded-For:</b> {forwarded_for}</p>
+    <p>Docker container port: 5000</p>
+    """
 
 @app.route("/health")
 def health():
